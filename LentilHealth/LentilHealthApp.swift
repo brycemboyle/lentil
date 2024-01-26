e5c2804448d9d8e1bb902415da42121dfa0c8765
Bryce Boyle <bryce@maggiewing.com>
2024-01-26 15:56:59 -0500

    Initial Commit

---
+++ LentilHealth/LentilHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  LentilHealthApp.swift
+//  LentilHealth
+//
+//  Created by Bryce Boyle on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LentilHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
